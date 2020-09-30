907dc28bfc2680179563ceeb12d7110693e82ed0
jysfc <63255178+jysfc@users.noreply.github.com>
2020-09-30 13:57:17 -0700

    add all cards

---
+++ script.js
@@ -0,0 +1,7 @@
+$("#save-card").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
+
+$("#back-to-answer").click(function () {
+   $("#overlay-error").toggleClass("d-flex d-none");
+});
