9c7ccd927c3c29c0474188c59e71c734275be3c5
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-14 23:04:32 -0700

    aded error msg if pw match email

--- script.js
+++ script.js
@@ -140,14 +140,20 @@ $("#top-edit-input, #bottom-edit-input").keyup(function () {
 
 });
 
+
 // lets go button
 $("#lets-go").click(function () {
    //email input
    const emailInput = $("#sign-up-email-input").val();
-   const email = emailInput.trim().toLowerCase();
-   const password = $("#sign-up-password-input").val();
+   let lowerCaseEmail = emailInput.trim().toLowerCase();
+   const passwordInput = $("#sign-up-password-input").val();
+   const lowerCasePassword = passwordInput.trim().toLowerCase();
+   const delimiter = `@`;
+      const indexOfEmail = lowerCaseEmail.indexOf(delimiter);
+      const localEmail = emailInput.slice(0, indexOfEmail);
+      
    // email error msg
-   if (email.length === 0) {
+   if (lowerCaseEmail.length === 0) {
       $("#sign-up-email-input").addClass("is-invalid");
       $("#sign-up-email-error").removeClass("d-none");
       $("#sign-up-email-error").html("Please enter your email address.");
@@ -157,26 +163,29 @@ $("#lets-go").click(function () {
       $("#sign-up-email-input").addClass("is-valid");
       $("#sign-up-email-error").addClass("d-none");
    }
-
    // password input
-   
-
-   if (password.length === 0) {
+   if (lowerCasePassword.length === 0) {
          $("#sign-up-password-input").addClass("is-invalid");
          $("#sign-up-password-error").removeClass("d-none");
          $("#sign-up-password-error").html("Please enter your password.");
-   } else if  (password.length <= 9) {
-         // password success msg
+   } else if  (lowerCasePassword.length <= 9) {
+   // password error msg
          $("#sign-up-password-input").addClass("is-invalid");
          $("#sign-up-password-error").removeClass("d-none");
          $("#sign-up-password-error").html("Your password must be at least 9 characters.");
-         } else {
-      // password success msg
+   } else {
+   // password success msg
          $("#sign-up-password-input").removeClass("is-invalid");
          $("#sign-up-password-input").addClass("is-valid");
          $("#sign-up-password-error").addClass("d-none");
-      }
 
-
-
+   } if (lowerCasePassword.includes(localEmail) && localEmail.length >= 4){
+   // pw match email error
+         $("#sign-up-password-input").addClass("is-invalid");
+         $("#sign-up-password-error").removeClass("d-none");
+         $("#sign-up-password-error").html("All or part of your email address cannot be used in your password.");
+   } 
 });
+
+
+// console.log(sentence.toLowerCase());
